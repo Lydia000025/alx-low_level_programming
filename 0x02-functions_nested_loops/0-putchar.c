0d389203ae21784f9a2a606fb4b38b903433837c
Lydia000025 <lydiafirew00@gmail.com>
2022-09-13 03:14:32 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main -this is main function
+ *@parameterx- description of parameter
+ *Description: function that prints out -putchar in new line
+ (*section header: section description)*
+ *Return: 0
+ */
+int main(void)
+	{
+		for  ( ; ; )
+		{
+			printf("_putchar %d" / n)
+		}
+		return (0);
+	}
