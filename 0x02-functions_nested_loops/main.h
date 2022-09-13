d447e8f8b7283b876ae32431d1992f8c070f40f0
Lydia000025 <lydiafirew00@gmail.com>
2022-09-13 15:07:39 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,18 @@
-#include <stdio.h>
-/**
- * main -this is main function
- *Description: function that prints out -putchar in new line
- *Return: 0
- */
-int main(void)
-{
-	char c;
+#ifndef main.h
+#define main.h
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	putchar('\n');
-	return (0);
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
+#endif
