7f7dc31576e52aac0e2101b90ec946001325c4fa
Lydia000025 <lydiafirew00@gmail.com>
2022-09-13 03:49:11 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+/**
+ * main -this is main function
+ *@parameterx- description of parameter
+ *Description: function that prints out -putchar in new line
+ (*section header: section description)*
+ *Return: 0
+ */
+int main(void)
+{
+	for  ( ; ; )
+	{
+		printf("_putchar" % d / n)
+	}
+	return (0);
+}
+
