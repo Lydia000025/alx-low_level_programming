be1dc654d88c40fda27e026712f7b094487ac691
Lydia000025 <lydiafirew00@gmail.com>
2022-09-14 12:25:30 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,11 +35,11 @@ void print_times_table(int n)
 				}
 				else
 				{
-				_putchar(((op / 100) % 10) + 48));
+				_putchar(((op / 100) % 10) + 48);
 				_putchar(((op / 10) % 10) + 48);
 				_putchar(((op % 10) + 48));
 				}
 			_putchar('\n');
 			}
-			}
+		}
 
