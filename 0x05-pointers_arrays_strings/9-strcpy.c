562ceb3b80265e1d83ad2ac431116580b23646e8
Lydia000025 <lydiafirew00@gmail.com>
2022-09-19 14:31:29 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string
+ * pointed
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = j;
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
