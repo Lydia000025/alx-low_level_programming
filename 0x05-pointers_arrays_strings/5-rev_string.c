8ed5eb32faa0069c61fabbf7a57cb49132508261
Lydia000025 <lydiafirew00@gmail.com>
2022-09-19 13:39:16 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,18 +12,16 @@ void rev_string(char *s)
 	
 	while (s[i])
 		i++;
-	
 	j = i / 2;
 	right = 0;
 
 	while (right != j)
 	{
 		char temp s[right];
-		int left = i -right - 1;
+		int left = i - right - 1;
 		
 		s[right] = s[left];
 		s[left] = temp;
-		
 		right++;
 	}
 }
