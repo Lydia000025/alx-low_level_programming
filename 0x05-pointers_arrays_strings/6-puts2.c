775116bdd0a2e13aaab465c07f319e7717a21996
Lydia000025 <lydiafirew00@gmail.com>
2022-09-19 13:59:20 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of 
+ * puts2 - a function that prints every other character of
  * a string, starting with the first character, followed by a new line.
  * @str: input parameter
  * Return: void
