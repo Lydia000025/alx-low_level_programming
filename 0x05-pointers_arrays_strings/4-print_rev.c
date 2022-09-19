b21f09825054b761a1b47dc2180aacbc0a621d32
Lydia000025 <lydiafirew00@gmail.com>
2022-09-19 12:06:34 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string
+ * in reverse, followed by a new line,
+ * @s: input parameter
+ * Return: void.
+ */
+void print_rev(char *s);
+{
+	int i = 0, j, right;
+
+	while (s[i])
+		i++;
+
+	j = i / 2;
+	right = 0;
+
+	while (right != j)
+	{
+		char temp s[right];
+		int left = i - right - 1;
+
+		s[right] = s[left];
+		s[left] = temp;
+
+		right++;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of 
+ * a string, starting with the first character, followed by a new line.
+ * @str: input parameter
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0, j, right;
+	
+	while (s[i])
+		i++;
+	
+	j = i / 2;
+	right = 0;
+
+	while (right != j)
+	{
+		char temp s[right];
+		int left = i -right - 1;
+		
+		s[right] = s[left];
+		s[left] = temp;
+		
+		right++;
+	}
+	
+}
+
