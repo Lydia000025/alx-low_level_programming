641bd888b51b6b229e1620633b97a88dbe441946
Lydia000025 <lydiafirew00@gmail.com>
2022-09-22 14:37:33 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int i = 0;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0' && n != j)
-	{
-		*(dest + i) = src[j];
-		j++;
-		i++;
-	}
-
-	*(dest + i) = '\0';
+	for (dest[j] != '\0'; j++)
+	for (src[j] != '\0' && i < n; i++)
+		dest[j + i] = src[j];
+	dest[j + i] = '\0';
 	return (dest);
 }
