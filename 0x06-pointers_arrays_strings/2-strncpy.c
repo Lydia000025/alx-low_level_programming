f6b6d5c6a122aa69eee2f449ac0e3d78cbc8924e
Lydia000025 <lydiafirew00@gmail.com>
2022-09-22 14:48:19 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "mian.h"
+
+/**
+ * -strncpy - coppies of input numbers
+ *  @dest: input 
+ *  @src: input 
+ *  @n: input
+ *  Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while(src[i++])
+		j++;
+			for (i = 0; src[i] && index < n; i++)
+				dest[i] = src[i];
+	for (i = j; i < n; i++;)
+		dest[i] = '\0';
+	return (dest);
+}
