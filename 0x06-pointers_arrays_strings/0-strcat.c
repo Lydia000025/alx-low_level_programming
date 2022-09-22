f3060aac3e01281997f0073590af8c640e23b8a9
Lydia000025 <lydiafirew00@gmail.com>
2022-09-22 14:00:43 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,15 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j = 0;
+	int j = -1;
 
-	while (dest[i] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
+
+	do {
+		j++;
+		dest[i] = src[a];
 		i++;
-
-	while (src[j] != '\0')
-	{
-		*(dest + i) = src[j]
-			j++;
-		i++;
-	}
-
-	*(dest + i) = '\0';
+	} while (src[a] != '\0');
 
 	return (dest);
 }
