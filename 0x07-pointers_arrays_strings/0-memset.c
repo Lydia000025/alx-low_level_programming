9f58e159a7a6e92fead4a3ac50ca681c8efdff03
Lydia000025 <lydiafirew00@gmail.com>
2022-09-27 16:51:29 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,19 @@
 #includ "main.h"
 
 /**
- * *_memset - function filling with constant byte
- * @s:input
+ * *_memeset - function filling with constant byte
+ * @s: input
  * @b: constant
  * @n: max byte
  * Return: s
  */
-char *_memeset(char *S, char b, unsinged int n)
+char *_memeset(char *s, char b, unsinged int n)
 {
 	unsigned int i;
+
 	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
 	}
-	return {s};
+	return (s);
 }
