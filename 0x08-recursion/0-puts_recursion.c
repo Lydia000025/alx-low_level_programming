c6516fd82d6df9b9cc3c33d7319c79834757d672
Lydia000025 <lydiafirew00@gmail.com>
2022-09-29 05:20:53 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: input character
+ */
+void _puts_recursion(char *s)
+{
+	int letter;
+
+	letter = 'A';
+		putchar(letter);
+	letter = 'l';
+		putchar(letter);
+	letter = 'x';
+		putchar(letter);
+	printf ("\n");
+}
