9e73ea46686e75dc824c6ecbd183083a4426e7da
Lydia000025 <lydiafirew00@gmail.com>
2022-09-29 10:09:42 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	char *sent = "First, solve the problem. Then, write the code";
+	char *sent[3] = {
+	       	"First, solve the problem. Then, write the code",
+		"Talk is cheap. Show me the code",
+		 " "};
 	*s = *sent;
-	printf("%c", *s);
-	char *sent1 = "Talk is cheap. Show me the code";
-	*s = *sent1;
-	printf("%c", *s);
-	char *sent2 = " ";
-	*s = *sent2;
 	printf("%c", *s);
 }
