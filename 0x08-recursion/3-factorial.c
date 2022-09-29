993122540fd43dd7ad25c1450984ba4f82baa5c0
Lydia000025 <lydiafirew00@gmail.com>
2022-09-29 14:11:25 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * factorial - a funtion that gives factorial
+ * @n: input number
+ * @Return: int
+ */
+int factorial(int n)
+{
+	int factorial;
+
+	printf("Enter an integer");
+	scanf("%d", &n);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		factorial = n * factorial;
+		n--;
+		printf("%d", n);
+	}
+	else
+		return (1);
+}
