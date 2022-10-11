32719908e00bfd2738362a128b61f6f90694ad66
Lydia000025 <lydiafirew00@gmail.com>
2022-10-10 17:14:04 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
-		d->age = age;
+		(*d).age = age;
 		d->owner = owner;
 	}
 }
