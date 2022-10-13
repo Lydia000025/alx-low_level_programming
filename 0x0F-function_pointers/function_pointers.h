12a3d58160b2f1da03da7aeb08e4710fc76884ff
Lydia000025 <lydiafirew00@gmail.com>
2022-10-12 19:02:37 -0700

    task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+/**
+ *function_pointers - function
+ *@name: input
+ *@f: input
+ */
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
