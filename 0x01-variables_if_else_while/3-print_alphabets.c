46778e1c30135e8d9d4349c9b9921ef3ae9ae92f
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-20 06:33:15 -0700

    task 2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,13 +4,15 @@ int mani(void)
 {
 	int l, u;
 	
-	if ( l < 122 || l > 97)
+	if( l > 97 && l < 122)
 	{
-		putchar(l);
-		u = l + 32;
-		putchar(u);
-		putchar('\n');
-	}
+		for(int i=0; i< (l - 32); i++)
+		{
+			putchar(l);
+			u = l + 32;
+			putchar(u);
+			putchar('\n');
+		}
 	return (0);
 }
 
