15fe37ad3abba5e5d45b81e9e971032e2a72fad7
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-20 04:53:33 -0700

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * void - function
+ */
+void print()
+{
+	printf("%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
+/**
+ * main - function
+ * Return: 1
+ */
+int main(void)
+{
+	print;
+	return (1);
+}
