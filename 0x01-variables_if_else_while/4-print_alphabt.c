280a1a4b2df7a9592f0fd03a622d95493a5fd172
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-20 07:07:30 -0700

    task 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+
+	for(l = 97; l < 123; l++)
+	{
+		if (l != 101 && l != 113)
+		{
+			putchar(l);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
