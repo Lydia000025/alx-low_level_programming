bbe269213dcfdc742b7468788c63b3d9c72e8294
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-20 19:43:26 -0700

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <math.h>
 /**
  * print_list - function
  * @h: linked list
@@ -8,11 +9,10 @@
 size_t print_list(const list_t *h)
 {
 	size_t num_of_nodes = 0;
-	
 	while (h)
 	{
 			if (h->str == NULL)
-				rintf("[0] (nil)\n");
+				printf("[0] (nil)\n");
 			else
 				printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
