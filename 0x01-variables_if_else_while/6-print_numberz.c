7a5d17bbbec606fc8f542dcbeb50f4605d6707f0
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-25 05:50:55 -0700

    task7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-	int n;
+	int n = '0';
 
-	for (n = 0; n < 10; n++)
+	while (n < 10)
 	{
 		putchar(n);
+		n++;
 	}
 	putchar('\n');
 	return (0);
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int main()
+{
+	char n = 'Gite';
+	int m = n%10;
+	
+	putchar(m);
+	return (0);
+}
