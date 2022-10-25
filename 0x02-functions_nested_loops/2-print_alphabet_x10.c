7a52c36665e516557fc91ae504b7259c55e211eb
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-25 10:04:04 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int j, i;
 	char n[] = "abcdefghijklmnopqrstuvwxyz";
-	
-	for (i = 0; i <26; i++)
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <26; i++)
 		{
 		putchar(n[i]);
 		}
+	}
 	putchar('\n');
 }
