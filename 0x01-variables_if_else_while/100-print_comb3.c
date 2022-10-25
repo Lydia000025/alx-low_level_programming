b8a203e5b54401ad2e9f169cca05f00eb0242985
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-25 07:25:39 -0700

    task 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,8 @@ int main()
 
 	for (n = 0; n < 10; n++)
 	{
-		putchar(n +'0');
-		while (k < 10)
-		{
-			putchar(k + '0');
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(n +'00');
 	}
-	putchar('\n');
 	return (0);
 }
 
