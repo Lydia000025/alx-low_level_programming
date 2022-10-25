d544e67a4e165f9c7ceb66aa329b010ac1e77e98
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-10-25 06:29:19 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -function
+ * Return: 0
+ */
+int main()
+{
+	int n;
+	
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	for (n = 'A'; n < 'G'; n++)
+	{
+		putchar(n)
+	}
+	purchar('\n');
+	return (0);
+}
