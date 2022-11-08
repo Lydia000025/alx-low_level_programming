d5b1ced4cf321b3b68dc4d46f26e95956f9d7969
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-11-08 03:40:15 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,20 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar("+1");
+		putchar('+');
+		putchar('1');
 		return(1);
 	}
 	else if (n < 0)
 	{
-		putchar("-1");
+		putchar('-');
+		putchar('1');
 		return (-1);
 	}
 	else 
 	{
-		putchar("00");
+		putchar('0');
+		putchar('0');
 		return (0);
 	}
 }
