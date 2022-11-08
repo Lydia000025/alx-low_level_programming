1fa87599acb75915bb390cf6b59803957e1a899d
lydiaabraham00 <lydiaabraham00@gmail.com>
2022-11-07 20:53:52 -0800

    task 1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,14 @@
-#include<stdio.h>
+#include "main.h"
+#include <stdio.h>
 /**
  * _islower - function
- * @c: int
- * Return: c
+ * @c: parameter
+ * return: 0
  */
 int _islower(int c)
 {
+	if (c < 'z' && c > 'a')
+		return (1);
+	else
+		return (0);
 }
